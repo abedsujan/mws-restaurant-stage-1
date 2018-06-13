53fbb256a86bc1c17882a70d0bd614f6269dca47
Abed Sujan <abedsujan@gmail.com>
2018-06-13 23:25:51 +0200

    feat: add browser-sunc

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,26 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
 
-
-gulp.task('default', defaultTask);
-
-gulp.task('styles', stylesTask);
-
-function defaultTask(done) {
-	// place code for your default task here
-	gulp.watch('sass/**/*.scss', gulp.series('styles'));
-
-	
-	// browserSync.init({
-	// 	server: "./"
-	// });
-
-	done();
-}
-
-
-function stylesTask() {
+gulp.task('styles', function stylesTask() {
 	return gulp.src('sass/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions']
 		}))
-		.pipe(gulp.dest('./css'));
-}
+		.pipe(gulp.dest('./css'))
+		.pipe(browserSync.stream());;
+});
+
+gulp.task('serve', gulp.series('styles', function () {
+	browserSync.init({
+		server: "./"
+	});
+	browserSync.stream();
+
+	gulp.watch('sass/**/*.scss', gulp.series('styles'));
+	gulp.watch("html/*.html").on('change', browserSync.reload);
+}));
+
+gulp.task('default', gulp.series('serve'));
